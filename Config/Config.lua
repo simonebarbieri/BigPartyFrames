328e3b959e6aa28a263297895925e6ba09719ec4
Simone Barbieri <simone.barbieri@hey.com>
2024-05-14 21:45:06 +0100

    Improved settings

--- Config/Config.lua
+++ Config/Config.lua
@@ -1,6 +1,4 @@
-local BPF_Config = BPF:NewModule("BPF_Config")
-
-local needReload = false
+BPF_Config = BPF:NewModule("BPF_Config")
 
 function BPF_Config:OnEnable()
     -- Create Menu
@@ -40,11 +38,10 @@ function BPF_Config:OnEnable()
                         type = 'toggle',
                         order = 3,
                         width = 'full',
-                        name = 'Remove Realm From Name (Requires ReloadUI)',
+                        name = 'Remove Realm From Name (*)',
                         desc = 'Remove the Realm name from the player\'s name',
                         set = function(_, val)
                             BPF_DB.remove_realm_name = val
-                            needReload = true
                         end,
                         get = function()
                             return BPF_DB.remove_realm_name
@@ -55,22 +52,16 @@ function BPF_Config:OnEnable()
                         name = 'Auras Options',
                         order = 10
                     },
-                    max_buff_debuff_desc = {
-                        type = 'description',
-                        name = 'These options require a reload of the ui.',
-                        order = 11
-                    },
                     max_party_buffs = {
                         type = 'range',
                         min = 3,
                         max = 20,
                         step = 1,
                         order = 12,
-                        name = 'Max Party Buffs',
+                        name = 'Max Party Buffs (*)',
                         desc = 'Set the maximum number of visible buffs',
                         set = function(_, val)
                             BPF_DB.max_party_buffs = val
-                            needReload = true
                         end,
                         get = function()
                             return BPF_DB.max_party_buffs
@@ -82,23 +73,19 @@ function BPF_Config:OnEnable()
                         max = 20,
                         step = 1,
                         order = 13,
-                        name = 'Max Party Debuffs',
+                        name = 'Max Party Debuffs (*)',
                         desc = 'Set the maximum number of visible debuffs',
                         set = function(_, val)
                             BPF_DB.max_party_debuffs = val
-                            needReload = true
                         end,
                         get = function()
                             return BPF_DB.max_party_debuffs
                         end
                     },
-                    reloadButton = {
-                        name = 'Reload UI',
-                        type = 'execute',
-                        hidden = not needReload,
-                        func = function()
-                            ReloadUI()
-                        end,
+                    reload_ui_warning = {
+                        type = 'description',
+                        name = '(*) These options require a reload of the ui.',
+                        order = -1
                     }
                 }
             }
@@ -107,10 +94,10 @@ function BPF_Config:OnEnable()
 
     -- Register Menu
     LibStub('AceConfig-3.0'):RegisterOptionsTable('Big Party Frames', options)
-    local BPF_Config = LibStub('AceConfigDialog-3.0'):AddToBlizOptions('Big Party Frames')
+    local BPF_ConfigPanel = LibStub('AceConfigDialog-3.0'):AddToBlizOptions('Big Party Frames')
 
     -- Register Slash Command
     BPF:RegisterChatCommand('BPF', function(_)
-        InterfaceOptionsFrame_OpenToCategory(BPF_Config)
+        InterfaceOptionsFrame_OpenToCategory(BPF_ConfigPanel)
     end)
 end
